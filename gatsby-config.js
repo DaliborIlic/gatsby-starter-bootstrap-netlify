bfdf547f42439b1514aed89d7e5b8f526dc547a9
DaliborIlic <dalibor.ilic.dado@gmail.com>
2019-04-24 09:17:04 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,  
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
