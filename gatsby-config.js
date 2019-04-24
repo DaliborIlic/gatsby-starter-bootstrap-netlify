1c18f0a300fe3d3a52b344c8a082e8ee83fcef90
DaliborIlic <dalibor.ilic.dado@gmail.com>
2019-04-24 09:54:57 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,27 @@ module.exports = {
         includePaths: [`${__dirname}/node_modules`, `${__dirname}/src/`],
         precision: 8
       }
-    }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // gatsby-remark-relative-images must
+          // go before gatsby-remark-images
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },    
   ]
 }
